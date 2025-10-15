96914c26110bd43303712c1f3433a9c3dad0face
emmanuel <emmanuel@gmail.com>
2025-10-15 12:25:37 +0200

    return 0 if invalid arg

--- ex00/ipssi_iterative_factorial.c
+++ ex00/ipssi_iterative_factorial.c
@@ -1,15 +1,12 @@
-#include <stdio.h>
 int ipssi_iterative_factorial(int nb){
+	if (nb < 0)
+{	return 0;
+}
 	int facto = 1;
 	int i = 1;
 	while (i <= nb){
 	facto *=i;
 	i++;
 }
-return facto;
-}
-
-int main(){
-
-printf("%d",ipssi_iterative_factorial(5));
+	return facto;
 }
