26f9030607f099addce7829314f592afe8421ffe
emmanuel <emmanuel@gmail.com>
2025-10-15 17:29:56 +0200

    norminetted

--- ex02/ipssi_recursive_power.c
+++ ex02/ipssi_recursive_power.c
@@ -6,10 +6,10 @@
 /*   By: Emmanuel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 17:07:28 by Emmanuel          #+#    #+#             */
-/*   Updated: 2025/10/15 17:22:35 by Emmanuel         ###   ########.fr       */
+/*   Updated: 2025/10/15 17:29:47 by Emmanuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+//NE FONCTIONNE PAS
 int	ipssi_recursive_power(int nb, int power)
 {
 	if (nb == 0 && power == 0)
@@ -18,4 +18,3 @@ int	ipssi_recursive_power(int nb, int power)
 	}
 	return (nb * ipssi_recursive_power(nb, power - 1));
 }
-//ne fonctionne pas
